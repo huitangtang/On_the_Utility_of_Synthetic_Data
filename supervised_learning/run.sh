313eeb463f9de046f5ea545e3c2e8f2a98213b71
HuiTang <42053362+huitangtang@users.noreply.github.com>
2023-03-10 15:16:06 +0800

    Update run.sh

--- supervised_learning/run.sh
+++ supervised_learning/run.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 python main.py  -a resnet50  --num_classes 10  --fix_dataset  --iters 2400000  --lr 0.01  --data_augmentation strong  --batch-size 128  --data_train /data/SynSL/  --data_eval /data/SynSL-120K/test_IID/ /data/SynSL-120K/test_IID_wo_BG/ /data/SynSL-120K/test_OOD/  --exp_name SynSL-PreTr  --seed 1  --evaluate_freq 20000
